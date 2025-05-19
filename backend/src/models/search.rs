dfd328b4e6c9100e84f6310383219aed34b4f6b9
Taha <tahakocabuga@gmail.com>
2025-05-19 13:27:11 +0300

    feat: add search model for response formatting in API

---
+++ backend/src/models/search.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize)]
+pub struct SearchRequest {
+    pub query: String,
+    pub media_type: MediaType,
+    pub page: Option<u32>,
+    pub limit: Option<u32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum MediaType {
+    Anime,
+    Manga,
+    Movie,
+    TvShow,
+    Game,
+}
+
+#[derive(Debug, Serialize)]
+pub struct SearchResponse {
+    pub results: Vec<SearchResult>,
+    pub total_results: u32,
+    pub total_pages: u32,
+    pub current_page: u32,
+}
+
+#[derive(Debug, Serailize)]
+pub struct SearchResult {
+    pub id: String,
+    pub title: String,
+    pub media_type: MediaType,
+    pub poster_path: Option<String>,
+    pub release_date: Option<String>,
+    pub rating: Option<f32>,
+    pub description: Option<String>,
+}
