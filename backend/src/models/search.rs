ab90de69376f3864b736b140fd16a5fa4d75899b
Taha <tahakocabuga@gmail.com>
2025-05-22 17:10:01 +0300

    feat: extend search model with additional fields and new data structures

--- backend/src/models/search.rs
+++ backend/src/models/search.rs
@@ -35,4 +35,47 @@ pub struct SearchResult {
     pub release_date: Option<String>,
     pub rating: Option<f32>,
     pub description: Option<String>,
+    pub slug: Option<String>,
+    pub rating_top: Option<i32>,
+    pub metacritic: Option<u32>,
+    pub playtime: Option<i32>,
+    pub genres: Option<Vec<Genre>>,
+    pub platforms: Option<Vec<Platform>>,
+    pub esrb_rating: Option<EsrbRating>,
+    pub tags: Option<Vec<Tag>>,
+    pub screenshots: Option<Vec<Screenshot>>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Genre {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Platform {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+}
+
+#[derive(Debug, Serialize, Deserialie)]
+pub struct EsrbRating {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Tag {
+    pub id: i32,
+    pub name: String,
+    pub slug: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Screenshot {
+    pub id: i32,
+    pub name: String,
 }
